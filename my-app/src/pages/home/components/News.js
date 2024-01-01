0daa069c6f421ca04600c1c711c99942bc079a6e
Fabrício Almeida <FabricioContato>
2024-01-01 17:56:00 -0300

    News area now displays information about how the webapp works

--- my-app/src/pages/home/components/News.js
+++ my-app/src/pages/home/components/News.js
@@ -1,18 +1,38 @@
 import React from "react";
 
 function News() {
-    return (
-      <div className="container">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida
-          auctor metus, vel euismod tellus vehicula quis. Sed in cursus dui. Sed
-          non maximus elit, vestibulum sollicitudin lorem. Vestibulum hendrerit,
-          turpis eget mollis auctor, nibh sapien vulputate sapien, non suscipit
-          risus ante eu nisi. Fusce in feugiat orci. Phasellus elementum elit
-          lacinia varius dignissim. Cras tortor mi, elementum non maximus eu,
-          fermentum vitae nisl.
-        </p>
-        <div className="row">
+  return (
+    <div className="container">
+      <ul>
+        <li>Create your online room and share the room link with a friend.</li>
+
+        <li>
+          Click on the ready button once you are prepared. Once both players are
+          ready, the match starts.
+        </li>
+
+        <li>
+          When the players leave the room, leaving it empty, the room is erased.
+        </li>
+
+        <li>
+          If a player leaves a room in the middle of a match, they can reconnect
+          using the same link as before, but they must use the same nickname as
+          before. Otherwise, the system will assume they are another player
+          trying to join a full room.
+        </li>
+
+        <li>
+          This website does not use cookies or any other kind of browser
+          storage.
+        </li>
+
+        <li>
+          This website is heavily inspired by jklm.fun . Visit the original
+          website.
+        </li>
+      </ul>
+      {/*  <div className="row">
           <a href="#" className="col-3">
             link1
           </a>
@@ -25,9 +45,9 @@ function News() {
           <a href="#" className="col-3">
             link4
           </a>
-        </div>
-      </div>
-    );
-  }
+        </div> */}
+    </div>
+  );
+}
 
-  export default News;
+export default News;
